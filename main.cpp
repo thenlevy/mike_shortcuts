d2210122d52e4d5784e4b402f3a98232894c785e
nlevy <nicolaspierrelevy@gmail.com>
2023-05-10 16:15:20 +0200

    Parse input

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+    long long n;
+    cin >> n;
+
+    vector<long long> a(n, 0);
+    for (int i = 0; i < n ; i++) {
+        cin >> a[i];
+    }
+
+}
