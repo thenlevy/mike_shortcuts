2e46843f46ab752b8209c9d8025b04e6bccbe5c7
nlevy <nicolaspierrelevy@gmail.com>
2023-05-10 17:01:19 +0200

    Comment and clean

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main() {
 
+    // parse input
     long long n;
     cin >> n;
 
@@ -18,40 +19,42 @@ int main() {
         dist_to_1[i] = i;
     }
 
-    long long nb_shortcuts_seen = 0;
-
+    // solve.
+    //
+    // The idea is to construt the set dist_i that contains all vertices at distance i of vertex 0.
+    // dist_1 = {1, a[0]} and dist_{i+1} = 
+    // {j +- 1, j in dist_i, j \notin dist_1, U ...  U dist_i} 
+    // U 
+    // {a[j], j in dist_i, j \notin dist_1, U ... U dist_i}
+    //
     long long i = 0;
     unordered_set<long long> at_dist_i = {};
-    unordered_set<long long> at_dist_i_plus_one = {};
     at_dist_i.emplace(0);
 
     while (i < n) {
-        //cout << "i = " << i << endl ;
-        at_dist_i_plus_one.clear();
+        unordered_set<long long> at_dist_i_plus_one = {};
 
         for (auto x: at_dist_i) {
-            //cout << "x = " << x << endl;
-            //cout << "try left" << endl;
+
+            // try to add x - 1. 
             if (x > 0) {
-                if (dist_to_1[x-1] >= i + 1) {
+                if (dist_to_1[x-1] >= i + 1) { // This test ensures that x-1 \notin dist_1,...dist_i (1)
                     dist_to_1[x-1] = i + 1;
                     at_dist_i_plus_one.emplace(x-1);
-                    //cout << "emplace" << x - 1 << endl;
-                }
-            }
-            //cout << "try right" << endl;
-            if (x < n - 1) {
-                if (dist_to_1[x+1] >= i + 1) {
-                    dist_to_1[x+1] = i + 1;
-                    at_dist_i_plus_one.emplace(x+1);
-                    //cout << "emplace" << x + 1 << endl;
                 }
             }
 
-            if (dist_to_1[a[x]] >= i + 1) {
+            // try to add x + 1
+            if (x < n - 1) {
+                if (dist_to_1[x+1] >= i + 1) { // see (1)
+                    dist_to_1[x+1] = i + 1;
+                    at_dist_i_plus_one.emplace(x+1);
+                }
+            }
+
+            if (dist_to_1[a[x]] >= i + 1) { // see (1)
                 dist_to_1[a[x]] = i + 1;
                 at_dist_i_plus_one.emplace(a[x]);
-                //cout << "emplace" << a[x] << endl;
             }
         }
 
@@ -59,11 +62,9 @@ int main() {
         at_dist_i = std::move(at_dist_i_plus_one);
     }
 
+    // output.
     for (size_t i = 0; i < n ; i ++) {
         cout << dist_to_1[i] << " ";
     }
     cout << endl;
-
-
-
 }
