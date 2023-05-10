9c7b900fab92f4d72b7395fb203767dc6799fd03
nlevy <nicolaspierrelevy@gmail.com>
2023-05-10 16:48:44 +0200

    Fix off by one error

--- main.cpp
+++ main.cpp
@@ -21,35 +21,42 @@ int main() {
     long long nb_shortcuts_seen = 0;
 
     long long i = 0;
-    unordered_set<long long> at_dist_i;
-    unordered_set<long long> at_dist_i_plus_one;
+    unordered_set<long long> at_dist_i = {};
+    unordered_set<long long> at_dist_i_plus_one = {};
     at_dist_i.emplace(0);
 
     while (i < n) {
+        //cout << "i = " << i << endl ;
         at_dist_i_plus_one.clear();
 
         for (auto x: at_dist_i) {
+            //cout << "x = " << x << endl;
+            //cout << "try left" << endl;
             if (x > 0) {
                 if (dist_to_1[x-1] > i + 1) {
                     dist_to_1[x-1] = i + 1;
                     at_dist_i_plus_one.emplace(x-1);
+                    //cout << "emplace" << x - 1 << endl;
                 }
             }
-            if (x < n) {
+            //cout << "try right" << endl;
+            if (x < n - 1) {
                 if (dist_to_1[x+1] > i + 1) {
                     dist_to_1[x+1] = i + 1;
                     at_dist_i_plus_one.emplace(x+1);
+                    //cout << "emplace" << x + 1 << endl;
                 }
             }
 
             if (dist_to_1[a[x]] > i + 1) {
                 dist_to_1[a[x]] = i + 1;
                 at_dist_i_plus_one.emplace(a[x]);
+                //cout << "emplace" << a[x] << endl;
             }
         }
 
         i++;
-        at_dist_i = at_dist_i_plus_one;
+        at_dist_i = std::move(at_dist_i_plus_one);
     }
 
     for (size_t i = 0; i < n ; i ++) {
